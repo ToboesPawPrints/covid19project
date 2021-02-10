0270dc077d9a4c62cea82ae788f4b67fe05c5b29
Kaleigh Deardorff <kaleigh.doodlepip@gmail.com>
2021-02-09 17:26:45 -0800

    Added map inline after testing.  Navbar in the works.

--- js/scripts.js
+++ js/scripts.js
@@ -1,54 +1,88 @@
-function createMap(){
-    const map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 38.00333, lng: -122.1341},
-    zoom: 12
+function initMap(){
+  // Map options
+  var options = {
+    zoom:11,
+    center:{lat: 38.00333, lng: -122.1341}
+    }
+
+    // New map
+    var map = new
+    google.maps.Map(document.getElementById('map'), options);
+
+    /*
+    // Add marker 
+    var marker = new google.maps.Marker({
+      position:{lat: 37.97814, lng: -122.04578},
+      map:map
     });
-  }
-
-function addMarker(props){
-  var marker = new google.maps.Marker({
-  position: props.coords,
-  map:map,
-}
-
-addMarker({
-  coords:{lat: 37.97814, lng: -122.04578},
-  content:'<h1>Lynn MA</h1>'
-}); //(1)Diablo Valley Cremation & Funeral Services
-
-addMarker({
-  coords:{lat: 38.00333, lng: -122.07258},
-  content:'<h1>Lynn MA</h1>'
-}); //(2)Alta Vista Cremation & Funeral Service 
-
-addMarker({
-  coords:{lat: 37.96710, lng: -121.99737}, 
-  content:'<h1>Lynn MA</h1>'
-}); //(3)Ouimet Bros. Concord Funeral Chapel
-
-addMarker({
-  coords:{lat: 37.89571, lng: -122.07803},
-  content:'<h1>Lynn MA</h1>'
-}); //(4) Hull's Walnut Creek Chapel
-
-addMarker({lat: 37.93170, lng: -122.06102}); //(5) Oak Park Hills Chapel
-addMarker({lat: 37.89426, lng: -122.05939}); //(6) Neptune Society of Northern California 
-addMarker({lat: 37.94745, lng: -122.10169}); //(7) Oakmont Memorial Park & Mortuary
-addMarker({lat: 37.94439, lng: -122.10643}); //(8) Queen of Heaven Catholic Cemetery & Mortuary
-//16.21 youtube video https://www.youtube.com/watch?v=Zxf1mnP5zcw&t=377s
-//Add Marker Function 
-function addMarker(props){
-var marker = new google.maps.Marker({
-  position: props.coords,
-  map:map,
-  }
-}
-//check content 
-if (props.content){
-  var infoWindow = new google.maps.InfoWindow({
-    content:props.content
-  });
+    
+    var infoWindow = new google.maps.InfoWindow({
+      content:'<h1>Lynn MA</h1>'
+    });
     marker.addListener('click', function(){
-      infoWindow.open(map, marker);
+    infoWindow.open(map, marker);
     });
+    */
+
+  addMarker({
+    coords:{lat: 37.97814, lng: -122.04578},
+    content:'<h1>Lynn MA</h1>'
+  }); //(1)Diablo Valley Cremation & Funeral Services
+
+  addMarker({
+    coords:{lat: 38.00333, lng: -122.07258},
+    content:'<h1>Lynn MA</h1>' 
+  }); //(2)Alta Vista Cremation & Funeral Service 
+
+  addMarker({
+    coords:{lat: 37.96710, lng: -121.99737}, 
+    content:'<h1>Lynn MA</h1>'
+  }); //(3)Ouimet Bros. Concord Funeral Chapel
+
+  addMarker({
+    coords:{lat: 37.89571, lng: -122.07803},
+    content:'<h1>Lynn MA</h1>'
+  }); //(4) Hull's Walnut Creek Chapel
+
+  addMarker({
+    coords:{lat: 37.93170, lng: -122.06102}, 
+    content: ''
+  });//(5) Oak Park Hills Chapel
+
+  addMarker({
+    coords:{lat: 37.89426, lng: -122.05939}, 
+    content: ''
+  });//(6) Neptune Society of Northern California
+
+  addMarker({
+    coords:{lat: 37.94745, lng: -122.10169}, 
+    content: ''
+  });//(7) Oakmont Memorial Park & Mortuary
+
+  addMarker({
+    coords:{lat: 37.94439, lng: -122.10643}, 
+    content: ''
+  });//(8) Queen of Heaven Catholic Cemetery & Mortuary
+
+
+    //Add Marker Function
+    function addMarker(props){
+      var marker = new google.maps.Marker({
+      position:props.coords,
+      map:map,
+      });
+
+      // Check content
+      if(props.content){
+        var infoWindow = new google.maps.InfoWindow({
+          content:props.content
+        });
+
+        marker.addListener('click', function(){
+        infoWindow.open(map, marker);
+        });
+      }
+    }
   } 
+
+
